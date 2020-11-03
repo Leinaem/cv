f6d055795ec6dfeb2d6ac29310b051045b5a575b
seb <saggoune@medialis.com>
2020-11-03 12:56:31 +0100

    fix errorPage link

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,14 +1,16 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Error = () => {
   return (
     <div className="error">
-    <div className="content">
-      <h1>404</h1>
-      <a href="/">
-        Accueil
-      </a>
-    </div>
+      <div className="content">
+        <h1>404</h1>
+        <p>Cette pas n'éxiste pas !</p>
+        <NavLink exact to="/" activeClassName="navActive">
+          <span className="pageLabel">Accueil</span>
+        </NavLink>
+      </div>
     </div>
   );
 };
