4207a04043c5ae9039138f088e13d83101eb5f24
seb <saggoune@medialis.com>
2020-11-03 12:26:14 +0100

    import initial
    react-app, homePage, errorPAge, sass

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import RouteList from "./pages/RouteList";
+import SideBar from "./components/SideBar";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="fullPageContainer">
+        <SideBar />
+        <div className="pageContent">
+          <Switch>
+            {RouteList.map((item, key) => (
+              <Route
+                path={item.path}
+                exact={item.exact}
+                component={item.component}
+                key={key}
+              />
+            ))}
+          </Switch>
+        </div>
+      </div>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
